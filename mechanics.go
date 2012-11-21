c36b594e19616ba0db1897bdb96bb3e34cce8634
Paul Coyle <paul@paulcoyle.com>
2012-11-20 22:39:28 -0700

    Made least rotations a static array.

--- mechanics.go
+++ mechanics.go
@@ -4,29 +4,15 @@ import (
 	"fmt"
 )
 
-func FindLeastRotation(A, B int) int {
-	var a [4][4]int
-	a[0][0] = 0;
-	a[0][1] = 1;
-	a[0][2] = 2;
-	a[0][3] = -1;
+var leastRotations = [4][4]int{
+	{0, 1, 2, -1},
+	{-1, 0, 1, 2},
+	{-2, -1, 0, 1},
+	{1, -2, -1, 0},
+}
 
-	a[1][0] = -1;
-	a[1][1] = 0;
-	a[1][2] = 1;
-	a[1][3] = 2;
-
-	a[2][0] = -2;
-	a[2][1] = -1;
-	a[2][2] = 0;
-	a[2][3] = 1;
-
-	a[3][0] = 1;
-	a[3][1] = -2;
-	a[3][2] = -1;
-	a[3][3] = 0;
-
-	return a[A][B]
+func FindLeastRotation(from, to int) int {
+	return leastRotations[from][to]
 }
 
 // For the given board, clears any full lines adjusting the board accordingly.
